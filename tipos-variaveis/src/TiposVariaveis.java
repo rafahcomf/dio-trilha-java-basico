49c6ad669bafe1dcc83916497c6b404505de75fc
rafahcomf <rafaelhinteriores@gmail.com>
2024-06-12 21:36:03 -0300

    exercicios e exemplos da aula 04 de tipos e variáveis, 12/06/2024

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,20 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500; // se fossem 2500 reais e 33 centavos, seria escrito 2500.33 por convenção americana//
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; // (short) casting - transformar uma em outra// 
+
+        int numero = 5;
+        numero = 10;
+
+        final double VALOR_DE_PI = 3.14; //determina uma constante;
+
+        
+
+
+
+        System.out.println(VALOR_DE_PI);
+    }
+}
